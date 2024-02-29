33d939463063d39323e513596423a6bdd0b4a59a
AhmedShahat012 <ggndgm@gmail.com>
2024-02-29 20:46:34 +0200

    change vaildation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-
 var productNameInput = document.getElementById("productNameInput");
 var productPriceInput = document.getElementById("productPriceInput");
 var productCatergryInput = document.getElementById("productCatergryInput");
-var productDescriptionInput = document.getElementById("productDescriptionInput");
+var productDescriptionInput = document.getElementById(
+  "productDescriptionInput"
+);
 var serechItems = document.getElementById("serechItems");
-var addproduct=document.getElementById("addproduct");
-var updateproduct=document.getElementById("updateproduct");
-var idexforUpdate=0;
+var addproduct = document.getElementById("addproduct");
+var updateproduct = document.getElementById("updateproduct");
+var idexforUpdate = 0;
 var allProduct = [];
 // check array is  not a empty and loaded data from localstorage to array and display it when wedoing refresh
 if (localStorage.getItem("allProduct") != null) {
@@ -14,20 +15,24 @@ if (localStorage.getItem("allProduct") != null) {
   displayData();
 }
 function addProduct() {
-    if(vailditionName()==true&&vailditionPrice()==true&&vailditioncategry()==true&&vailditiondesc()==true){
-        var product = {
-            Name: productNameInput.value,
-            price: productPriceInput.value,
-            categry: productCatergryInput.value,
-            desc: productDescriptionInput.value,
-          };
-        
-          allProduct.push(product);
-          localStorage.setItem("allProduct", JSON.stringify(allProduct)); //storage array in localsotage and JSON.stringify(allProduct) conver all arr to one string
-          displayData();
-          clearData();
-    }
- 
+  if (
+    vailditionName() == true &&
+    vailditionPrice() == true &&
+    vailditioncategry() == true &&
+    vailditiondesc() == true
+  ) {
+    var product = {
+      Name: productNameInput.value,
+      price: productPriceInput.value,
+      categry: productCatergryInput.value,
+      desc: productDescriptionInput.value,
+    };
+
+    allProduct.push(product);
+    localStorage.setItem("allProduct", JSON.stringify(allProduct)); //storage array in localsotage and JSON.stringify(allProduct) conver all arr to one string
+    displayData();
+    clearData();
+  }
 }
 
 function clearData() {
@@ -58,12 +63,12 @@ function deletdata(idx) {
   localStorage.setItem("allProduct", JSON.stringify(allProduct)); // here we call it when you delet from website storege from first to delet data from localsotarge
   displayData();
 }
-function serechProduct(){
-    var term= serechItems.value;
-    var items=``
-    for (var i = 0; i < allProduct.length; i++) {
-        if (allProduct[i].Name.toLowerCase().includes(term.toLowerCase()) == true) {
-    items += `
+function serechProduct() {
+  var term = serechItems.value;
+  var items = ``;
+  for (var i = 0; i < allProduct.length; i++) {
+    if (allProduct[i].Name.toLowerCase().includes(term.toLowerCase()) == true) {
+      items += `
     <tr>
     <td>${allProduct[i].Name}</td>
     <td>${allProduct[i].price}</td>
@@ -73,123 +78,100 @@ function serechProduct(){
     <td><button onclick="deletdata(${i}) " class="btn btn-success">delet</button></td>
     </tr>
      `;
-}
-}
-document.getElementById("tablebody").innerHTML = items;
+    }
+  }
+  document.getElementById("tablebody").innerHTML = items;
 }
 function setDate(indx) {
-    idexforUpdate=indx;
-    var data=allProduct[indx];
-    productNameInput.value=data.Name;
-    productPriceInput.value=data.price;
-    productCatergryInput.value=data.categry;
-    productDescriptionInput.value=data.desc;
- 
+  idexforUpdate = indx;
+  var data = allProduct[indx];
+  productNameInput.value = data.Name;
+  productPriceInput.value = data.price;
+  productCatergryInput.value = data.categry;
+  productDescriptionInput.value = data.desc;
 
-    addproduct.classList.add("d-none")
-    updateproduct.classList.remove("d-none")
+  addproduct.classList.add("d-none");
+  updateproduct.classList.remove("d-none");
 }
 
-function updatfun(){
-    var product = {
+function updatfun() {
+  var product = {
     Name: productNameInput.value,
     price: productPriceInput.value,
     categry: productCatergryInput.value,
     desc: productDescriptionInput.value,
   };
-  allProduct.splice(idexforUpdate,1,product);
+  allProduct.splice(idexforUpdate, 1, product);
   localStorage.setItem("allProduct", JSON.stringify(allProduct));
   displayData();
   addproduct.classList.remove("d-none");
-updateproduct.classList.add("d-none");
+  updateproduct.classList.add("d-none");
+  clearData();
 }
 
+function vailditionName() {
+  var massageName = document.getElementById("NameInput");
+  var regexName = /^[A-Z][a-zA-Z ]+$/;
+  var text = productNameInput.value;
+  if (regexName.test(text) == true) {
+    productNameInput.classList.add("is-valid");
+    productNameInput.classList.remove("is-invalid");
+    massageName.classList.add("d-none");
+    return true;
+  } else {
+    productNameInput.classList.add("is-invaild");
+    productNameInput.classList.remove("is-vaild");
+    massageName.classList.remove("d-none");
+    return false;
+  }
+}
+function vailditionPrice() {
+  var massageprice = document.getElementById("PriceInput");
+  var regexName = /^([1-9]\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/;
+  var text = productPriceInput.value;
+  if (regexName.test(text) == true) {
+    productPriceInput.classList.add("is-valid");
+    productPriceInput.classList.remove("is-invalid");
+    massageprice.classList.add("d-none");
+    return true;
+  } else {
+    productPriceInput.classList.add("is-invaild");
+    productPriceInput.classList.remove("is-vaild");
+    massageprice.classList.remove("d-none");
+    return false;
+  }
+}
+function vailditioncategry() {
+  var massagecategry = document.getElementById("categryInput");
+  var regexName = /^[a-z]{3,10}$/;
+  var text = productCatergryInput.value;
+  if (regexName.test(text) == true) {
+    productCatergryInput.classList.add("is-valid");
+    productCatergryInput.classList.remove("is-invalid");
+    massagecategry.classList.add("d-none");
 
+    return true;
+  } else {
+    productCatergryInput.classList.add("is-invaild");
+    productCatergryInput.classList.remove("is-vaild");
+    massagecategry.classList.remove("d-none");
+    return false;
+  }
+}
+function vailditiondesc() {
+  var massagedesc = document.getElementById("descInput");
+  var regexName = /^[a-zA-Z ]+$/;
+  var text = productDescriptionInput.value;
+  if (regexName.test(text) == true) {
+    productDescriptionInput.classList.add("is-valid");
+    productDescriptionInput.classList.remove("is-invalid");
+    massagedesc.classList.add("d-none");
 
-function vailditionName(){
-    var massageName=document.getElementById("NameInput");
-    var regexName=/^[A-Z][a-z]{3,8}$/
-    var text=productNameInput.value;
-    if(regexName.test(text)==true){
-        productNameInput.classList.add("is-valid");
-        productNameInput.classList.remove("is-invalid")
-        massageName.classList.add("d-none")
-        return true
-    }
-    else{
-        productNameInput.classList.add("is-invaild")
-        productNameInput.classList.remove("is-vaild")
-        massageName.classList.remove("d-none")
-        return false
-       
-
-    }
-
-    
-} 
-function vailditionPrice(){
-    var massageprice=document.getElementById("PriceInput");
-    var regexName=/^[1-9]{3,5}$/
-    var text=productPriceInput.value;
-    if(regexName.test(text)==true){
-        productPriceInput.classList.add("is-valid");
-        productPriceInput.classList.remove("is-invalid")
-        massageprice.classList.add("d-none")
-        return true
-    }
-    else{
-        productPriceInput.classList.add("is-invaild")
-        productPriceInput.classList.remove("is-vaild")
-        massageprice.classList.remove("d-none")
-        return false
-       
-
-    }
-
-    
-} 
-function vailditioncategry(){
-    var massagecategry=document.getElementById("categryInput");
-    var regexName=/^[a-z]{3,10}$/
-    var text=productCatergryInput.value;
-    if(regexName.test(text)==true){
-        productCatergryInput.classList.add("is-valid");
-        productCatergryInput.classList.remove("is-invalid")
-        massagecategry.classList.add("d-none")
-      
-        return true
-    }
-    else{
-        productCatergryInput.classList.add("is-invaild")
-        productCatergryInput.classList.remove("is-vaild")
-        massagecategry.classList.remove("d-none")
-        return false
-       
-
-    }
-
-    
-} 
-function vailditiondesc(){
-    var massagedesc=document.getElementById("descInput");
-    var regexName=/^[a-z]{3,30}$/
-    var text=productDescriptionInput.value;
-    if(regexName.test(text)==true){
-        productDescriptionInput.classList.add("is-valid");
-        productDescriptionInput.classList.remove("is-invalid")
-        massagedesc.classList.add("d-none")
-      
-        return true
-    }
-    else{
-        productDescriptionInput.classList.add("is-invaild")
-        productDescriptionInput.classList.remove("is-vaild")
-        massagedesc.classList.remove("d-none")
-        return false
-       
-
-    }
-
-    
-} 
-
+    return true;
+  } else {
+    productDescriptionInput.classList.add("is-invaild");
+    productDescriptionInput.classList.remove("is-vaild");
+    massagedesc.classList.remove("d-none");
+    return false;
+  }
+}
